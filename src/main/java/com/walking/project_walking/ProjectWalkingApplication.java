f6253fb22a01f0c4cd66881e0b666bff132bcb28
pc <gkandpwpjp@gmail.com>
2024-10-28 13:47:12 +0900

    [#06 ADD] Test

--- src/main/java/com/walking/project_walking/ProjectWalkingApplication.java
+++ src/main/java/com/walking/project_walking/ProjectWalkingApplication.java
@@ -2,6 +2,7 @@ package com.walking.project_walking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectWalkingApplication {
