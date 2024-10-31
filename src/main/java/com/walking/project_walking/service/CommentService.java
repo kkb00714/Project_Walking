af5055e163553f399364b8cb89e84a0efae800d4
seohyun <shanan9623@gmail.com>
2024-10-31 14:57:40 +0900

    Add reply functionality

--- src/main/java/com/walking/project_walking/service/CommentService.java
+++ src/main/java/com/walking/project_walking/service/CommentService.java
@@ -2,7 +2,7 @@ package com.walking.project_walking.service;
 
 import lombok.*;
 import com.walking.project_walking.domain.Comments;
-import com.walking.project_walking.domain.dto.AddCommentRequest;
+import com.walking.project_walking.domain.dto.CommentRequest;
 import com.walking.project_walking.domain.dto.CommentResponse;
 import com.walking.project_walking.repository.CommentsRepository;
 import org.springframework.stereotype.Service;
@@ -17,17 +17,19 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    public CommentResponse createComment (AddCommentRequest request) {
+    public CommentResponse createComment (CommentRequest request) {
         Comments comment = Comments.builder()
                 .postId(request.getPostId())
                 .userId(request.getUserId())
                 .content(request.getContent())
+                .parentCommentId(request.getParentCommentId())
                 .build();
 
         Comments savedComment = commentRepository.save(comment);
 
         return new CommentResponse(
                 savedComment.getCommentId(),
+                savedComment.getParentCommentId(),
                 savedComment.getPostId(),
                 savedComment.getUserId(),
                 savedComment.getContent(),
@@ -42,7 +44,7 @@ public class CommentService {
         commentRepository.save(comment);
     }
 
-    public CommentResponse modifyComment(Long commentId, AddCommentRequest request) {
+    public CommentResponse modifyComment(Long commentId, CommentRequest request) {
         Comments comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
         comment.setContent(request.getContent());
@@ -50,6 +52,7 @@ public class CommentService {
 
         return new CommentResponse(
                 updatedComment.getCommentId(),
+                updatedComment.getParentCommentId(),
                 updatedComment.getPostId(),
                 updatedComment.getUserId(),
                 updatedComment.getContent(),
